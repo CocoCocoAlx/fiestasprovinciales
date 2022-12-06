0b4b32a7b2f2f99da98d9c5f27ecad3635f9364a
CocoCocoAlx <vania182004@gmail.com>
2022-12-05 21:35:52 -0300

    Initial commit

---
+++ src/main/java/com/polotic/fiestasprovinciales/FiestasprovincialesApplication.java
@@ -0,0 +1,13 @@
+package com.polotic.fiestasprovinciales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiestasprovincialesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiestasprovincialesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polotic/fiestasprovinciales/FiestasprovincialesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polotic.fiestasprovinciales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiestasprovincialesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
